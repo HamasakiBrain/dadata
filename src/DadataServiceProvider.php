c02dddfa085406643c6181bb3c4e18cf65ffe927
Amon <herovuwbdal@gmail.com>
2022-04-13 14:39:57 +0300

    Xyi 2

--- src/DadataServiceProvider.php
+++ src/DadataServiceProvider.php
@@ -35,6 +35,6 @@ class DadataServiceProvider extends ServiceProvider
             return new ClientClean();
         });
 
-        $this->mergeConfigFrom(__DIR__.'/../config/dadata.php', 'dadata');
+        $this->mergeConfigFrom(__DIR__ . '/../config/dadata.php', 'dadata');
     }
 }
